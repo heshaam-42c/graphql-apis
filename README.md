bc38975f50306fffd1d906ca75235fe81191243a
heshaam-42c <heshaam.attar@42crunch.com>
2025-03-08 10:45:51 -0800

    update script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # graphql-apis
 
-Run with output to JSON file:
+Run with output to audit/report.json file:
 
 ```
 ./graphql-audit.sh apis/vortex.graphql
--- graphql-audit.sh
+++ graphql-audit.sh
@@ -4,5 +4,5 @@ echo "$#"
 echo "$1"
 
 if [ $# -gt 0 ]; then
-    ~/.42crunch/bin/42c-ast graphql audit -o report.json $1
+    ~/.42crunch/bin/42c-ast graphql audit -o audit/report.json $1
 fi
